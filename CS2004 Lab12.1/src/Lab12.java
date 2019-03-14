da8d449ac37f04126e9f2fce599f68287b03f6d1
KevinD-1305 <1641539@brunel.ac.uk>
2019-03-14 17:12:50 +0000

    commmit

---
+++ CS2004 Lab12.1/src/Lab12.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+public class Lab12 
+
+{
+	public static void main(String args[])
+	{
+		//Read in the weights
+		//Make sure you change the filename as appropriate!
+		//This will only work if "c:\temp\1000 Primes.txt" exists!
+		ArrayList<Double> w = CS2004.ReadNumberFile("h:\\temp\\1000 Prime.txt");
+		//Set the weights
+		//Run 10 repeats
+		for(int i=0;i<10;++i)
+		{
+				//Reset the fitness count
+			OneMaxChrome.ClearFC();
+			//The following parameters are not very good!
+			//These are the ones you should try and optimise!
+			int popsize = 10;
+			double mrate = 0.002;
+			double crate = 0.640;
+			//You will not need to change the following
+			SimpleGeneticAlgorithm ga = new SimpleGeneticAlgorithm(popsize,10,1000,mrate,crate);
+			//Run the GA for 10,000 function calls
+			double f = ga.RunSGA(10000,false).GetFitness();//Changed from false
+			System.out.println(f);
+		}
+	}
+}
+
